2be469164876bb90a4b70a0aee8196068a558625
TheWhiteHackers <nurefsanbera@gmail.com>
2023-01-28 19:56:29 -0600

    small touches

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Profile_Search
+
+## This project later turned into a employee finder
+## Used to find info on your empolees (your role is an manager)
+### Note: These aren't really people and the into is made up
--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById("sbtn")
 const text = document.getElementById("stext")
-
+const profilebox = document.querySelector(".profile")
 
 
 
@@ -8,17 +8,56 @@ text.addEventListener("keyup", (event)=>{
     
     let textval = event.target.value
     if (textval !== ""){
-        console.log(textval);
+        // console.log(textval);
+        fetchprofile(textval);
     }
     
-    
 })
+
+function showprofile(profile){
+    console.log("data came!", profile);
+    profilebox.innerHTML=`
+    <div class="card mt-4" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Contact Information</h5>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Username: ${profile.username}</li>
+      <li class="list-group-item">Name: ${profile.name}</li>
+      <li class="list-group-item">Email: ${profile.email}</li>
+      <li class="list-group-item">Phone Number: ${profile.phone}</li>
+      <li class="list-group-item">Address: ${profile.address.city}, ${profile.address.street}, ${profile.address.suite}, ${profile.address.zipcode}</li>
+      <li class="list-group-item">Company: ${profile.company.name}</li>
+      <li class="list-group-item">Catch Phrase: ${profile.company.catchPhrase}</li>
+      
+
+    </ul>
+  </div>`
+};
+
 
 function fetchprofile(username){
     const profile = fetch(`https://jsonplaceholder.typicode.com/users?username=${username}`)
     .then(res=>res.json())
     .then(data=>{
-        console.log(data);
-})}
+        console.log("Data:", data);
+        
+        const checker = document.getElementById("checker")
+        if (data.length === 0){
+          checker.innerHTML="<p>This username doesn't exist</p>"
+      } else {
+          showprofile(data[0]);
+          checker.innerHTML="";
+      }
+   
+});}
 
-console.log(fetchprofile("Bret"));
+// console.log(fetchprofile("Bret"));
+
+
+
+
+
+
+
